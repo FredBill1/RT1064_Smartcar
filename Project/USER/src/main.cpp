fe18811fb28aa6e64cb98161c1346ade3513063a
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-01-08 15:10:51 +0800

    成功导入eigen3.4；之前不成功是因为逐飞移植的rtt没有把libc添加到工程里

--- Project/USER/src/main.cpp
+++ Project/USER/src/main.cpp
@@ -3,20 +3,36 @@ extern "C" {
 int main(void);
 }
 
+#include <Eigen/Eigen>
+
 #include "SerialIO.hpp"
 
 SerialIO wireless;
 
 double aa, bb, cc;
 
+template <typename T> inline void showMat(T& x) {
+    for (int i = 0; i < x.rows(); ++i) {
+        for (int j = 0; j < x.cols(); ++j) rt_kprintf("%d ", x(i, j));
+        rt_kputs("\n\r");
+    }
+}
+
 int main(void) {
     //此处编写用户代码(例如：外设初始化代码等)
 
     gpio_init(B9, GPO, 0, GPIO_PIN_CONFIG);
+    // rt_thread_mdelay(500);
 
     wireless.init("wireless", WIRELESS_UART, WIRELESS_UART_BAUD, WIRELESS_UART_TX, WIRELESS_UART_RX);
-
     EnableGlobalIRQ(0);
+
+    Eigen::Matrix<int, 3, 3> x;
+    x << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+    auto y = x.adjoint();
+    showMat(x);
+    showMat(y);
+    showMat(x * y);
 
     while (1) {
         //此处编写需要循环执行的代码
