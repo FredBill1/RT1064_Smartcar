e4921f31c32b9595aa5c4bbfc905890df2129648
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-01-09 12:56:28 +0800

    用来测试无线串口上位机的程序

--- Project/USER/src/main.cpp
+++ Project/USER/src/main.cpp
@@ -3,20 +3,12 @@ extern "C" {
 int main(void);
 }
 
-#include <Eigen/Eigen>
+#include <limits>
 
 #include "SerialIO.hpp"
-
 SerialIO wireless;
 
-double aa, bb, cc;
-
-template <typename T> inline void showMat(T& x) {
-    for (int i = 0; i < x.rows(); ++i) {
-        for (int j = 0; j < x.cols(); ++j) rt_kprintf("%d ", x(i, j));
-        rt_kputs("\n\r");
-    }
-}
+double a = 1e9 + 7, b = 1e18, c = std::numeric_limits<double>::max();
 
 int main(void) {
     //此处编写用户代码(例如：外设初始化代码等)
@@ -27,22 +19,12 @@ int main(void) {
     wireless.init("wireless", WIRELESS_UART, WIRELESS_UART_BAUD, WIRELESS_UART_TX, WIRELESS_UART_RX);
     EnableGlobalIRQ(0);
 
-    Eigen::Matrix<int, 3, 3> x;
-    x << 1, 2, 3, 4, 5, 6, 7, 8, 9;
-    auto y = x.adjoint();
-    showMat(x);
-    showMat(y);
-    showMat(x * y);
-
     while (1) {
         //此处编写需要循环执行的代码
-        wireless.waitHeader();
-        if (wireless.readD(aa, bb, cc)) {
-            rt_kprintf("%d %d %d\n", (int)aa, (int)bb, (int)cc);
-            wireless.sendHeader();
-            wireless.writeD(aa, bb, cc);
-        } else
-            rt_kputs("error\n");
+        wireless.sendHeader();
+        wireless.putchar(3);
+        wireless.writeD(a, b, c);
         gpio_toggle(B9);
+        rt_thread_mdelay(1000);
     }
 }
