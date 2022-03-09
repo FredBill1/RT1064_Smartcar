a73197f2c87d0fb19e596c162bafbd55c606117b
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-09 10:24:41 +0800

    优化并查集

--- Project/CODE/components/imgProc/inc/apriltag/internal/Unionfind.hpp
+++ Project/CODE/components/imgProc/inc/apriltag/internal/Unionfind.hpp
@@ -1,6 +1,7 @@
 #ifndef _apriltag_Unionfind_hpp
 #define _apriltag_Unionfind_hpp
 
+#include <algorithm>
 #include <cstdint>
 
 namespace imgProc {
@@ -11,20 +12,22 @@ template <typename T, int_fast32_t Size> class Unionfind {
     using data_t = T;
 
  protected:
-    data_t fa[Size];
-    int32_t sz[Size];
+    struct Node {
+        data_t fa;
+        int32_t sz;
+    } node[Size];
 
  public:
     inline void reset() {
-        for (int_fast32_t i = 0; i < Size; ++i) sz[i] = 1, fa[i] = i;
+        for (int_fast32_t i = 0; i < Size; ++i) node[i].sz = 1, node[i].fa = i;
     }
     inline data_t find(data_t x) {
-        if (fa[x] == x) return x;
+        if (node[x].fa == x) return x;
         data_t t = x;
-        while (t != fa[t]) t = fa[t];
-        while (x != fa[x]) {
-            data_t u = fa[x];
-            fa[x] = t;
+        while (t != node[t].fa) t = node[t].fa;
+        while (x != node[x].fa) {
+            data_t u = node[x].fa;
+            node[x].fa = t;
             x = u;
         }
         return x;
@@ -32,11 +35,12 @@ template <typename T, int_fast32_t Size> class Unionfind {
     inline void merge(data_t x, data_t y) {
         data_t xr = find(x), yr = find(y);
         if (xr == yr) return;
-        fa[xr] = yr;
-        sz[yr] += sz[xr];
+        if (node[xr].sz > node[yr].sz) std::swap(xr, yr);
+        node[xr].fa = yr;
+        node[yr].sz += node[xr].sz;
     }
     inline data_t operator[](data_t x) { return find(x); }
-    inline int32_t size(data_t x) { return sz[find(x)]; }
+    inline int32_t size(data_t x) { return node[find(x)].sz; }
 };
 
 }  // namespace apriltag
