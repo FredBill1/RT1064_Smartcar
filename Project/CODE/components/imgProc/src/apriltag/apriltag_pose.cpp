ab2d1e9467417a61c87fa1d326d4039e3a2e2932
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-10 19:16:28 +0800

    坐标系改对了

--- Project/CODE/components/imgProc/src/apriltag/apriltag_pose.cpp
+++ Project/CODE/components/imgProc/src/apriltag/apriltag_pose.cpp
@@ -9,14 +9,14 @@ namespace apriltag {
 void estimate_pose_for_tag_homography(apriltag_detection_info& info, apriltag_pose& solution) {
     auto& R = solution.R;
     auto& t = solution.t;
-    homography_to_pose(info.det->H, info.fx, info.fy, info.cx, info.cy, R, t);
+    homography_to_pose(info.det->H, -info.fx, info.fy, info.cx, info.cy, R, t);
 
     float_t scale = info.tagsize / 2;
     rep(i, 0, 3) t[i] *= scale;
-    // req(i, 1, 2) {
-    //     t[i] = -t[i];
-    //     rep(j, 0, 3) R[i][j] = -R[i][j];
-    // }
+    req(i, 1, 2) {
+        t[i] = -t[i];
+        rep(j, 0, 3) R[j][i] = -R[j][i];
+    }
 }
 
 }  // namespace apriltag
