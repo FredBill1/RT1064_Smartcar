981b0b54e082ea4c9e9b1d0e765d1fb8e54f588e
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-16 18:44:22 +0800

    直接用算式多好

--- Project/USER/inc/ApriltagConfig.hpp
+++ Project/USER/inc/ApriltagConfig.hpp
@@ -4,9 +4,9 @@
 namespace imgProc {
 namespace apriltag {
 
-constexpr int maxhamming = 1;              // 解码tag时最多错几位
-constexpr float_t tagsize = 0.1145454545;  // apriltag的边长，单位是米
-constexpr float_t shift_dist = 0.05;       // 3维信息转二维坐标时tag平面向后方平移的距离
+constexpr int maxhamming = 1;               // 解码tag时最多错几位
+constexpr float_t tagsize = 0.18 * 7 / 11;  // apriltag的边长，单位是米
+constexpr float_t shift_dist = 0.05;        // 3维信息转二维坐标时tag平面向后方平移的距离
 
 // 相机标定参数
 constexpr float_t fx = 397.38711548;  // In pixels.
