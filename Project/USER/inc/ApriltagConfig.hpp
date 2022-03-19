2bb6b044f775d4dd9ae0a9b952cd53e9e43966f2
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-19 21:18:43 +0800

    换了个90度镜头 重新标定

--- Project/USER/inc/ApriltagConfig.hpp
+++ Project/USER/inc/ApriltagConfig.hpp
@@ -9,10 +9,10 @@ constexpr float_t tagsize = 0.18 * 7 / 11;  // apriltag的边长，单位是米
 constexpr float_t shift_dist = 0.05;        // 3维信息转二维坐标时tag平面向后方平移的距离
 
 // 相机标定参数
-constexpr float_t fx = 397.38711548;  // In pixels.
-constexpr float_t fy = 399.70840454;  // In pixels.
-constexpr float_t cx = 369.06676129;  // In pixels.
-constexpr float_t cy = 226.30497101;  // In pixels.
+constexpr float_t fx = 589.70233154;  // In pixels.
+constexpr float_t fy = 648.32983398;  // In pixels.
+constexpr float_t cx = 376.15877696;  // In pixels.
+constexpr float_t cy = 242.75541548;  // In pixels.
 
 }  // namespace apriltag
 }  // namespace imgProc
