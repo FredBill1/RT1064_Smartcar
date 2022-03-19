e9ab306038b4722f366182bef9c178025cd3f451
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-19 13:13:49 +0800

    弄了显示err的函数，删了cube显示id的接口

--- Project/CODE/components/imgProc/inc/apriltag/visualization_pose.hpp
+++ Project/CODE/components/imgProc/inc/apriltag/visualization_pose.hpp
@@ -8,9 +8,11 @@ namespace apriltag {
 
 void plot_pose_axis(uint8_t* img, const apriltag_detection_info& info, const apriltag_pose& pose);
 
-void plot_pose_cube(uint8_t* img, const apriltag_detection_info& info, const apriltag_pose& pose, bool plotID = true);
+void plot_pose_cube(uint8_t* img, const apriltag_detection_info& info, const apriltag_pose& pose);
 
 void plot_det_poses(uint8_t* img, const apriltag_detection_info& info, const det_poses_t& det_poses);
+
+void polt_pose_err(uint8_t* img, const apriltag_detection& det, const apriltag_pose& pose);
 
 }  // namespace apriltag
 }  // namespace imgProc
--- Project/CODE/components/imgProc/src/apriltag/visualization_pose.cpp
+++ Project/CODE/components/imgProc/src/apriltag/visualization_pose.cpp
@@ -29,7 +29,7 @@ void plot_pose_axis(uint8_t* img, const apriltag_detection_info& info, const apr
     plotImg(img, p0[1], p0[0], colors[3], 1);
 }
 
-void plot_pose_cube(uint8_t* img, const apriltag_detection_info& info, const apriltag_pose& pose, bool plotID) {
+void plot_pose_cube(uint8_t* img, const apriltag_detection_info& info, const apriltag_pose& pose) {
     float_t p[8][2];
     const float_t X = info.tagsize / 2;
     float_t corner[4][3]{{X, X}, {X, -X}, {-X, -X}, {-X, X}};
@@ -39,20 +39,20 @@ void plot_pose_cube(uint8_t* img, const apriltag_detection_info& info, const apr
         tag_pose_to_image(info, pose, corner[i], p[i + 4]);
     }
     rep(i, 0, 4) lineImg(img, p[i][1], p[i][0], p[((i + 1) & 3)][1], p[((i + 1) & 3)][0], colors[2], 0);
-    if (plotID) {
-        float_t c[2], center[3]{0, 0, -X};
-        tag_pose_to_image(info, pose, center, c);
-        plotInt(img, c[1], c[0], info.det->id, 2, true);
-    }
     rep(i, 0, 4) lineImg(img, p[i][1], p[i][0], p[i + 4][1], p[i + 4][0], colors[1], 0);
     rep(i, 0, 4) lineImg(img, p[i + 4][1], p[i + 4][0], p[((i + 1) & 3) + 4][1], p[((i + 1) & 3) + 4][0], colors[0], 0);
 }
 
+void polt_pose_err(uint8_t* img, const apriltag_detection& det, const apriltag_pose& pose) {
+    plotInt(img, det.c[1], det.c[0], pose.err * 1e9, 4, true);
+}
+
 void plot_det_poses(uint8_t* img, const apriltag_detection_info& info, const det_poses_t& det_poses) {
     for (auto& [det, pose] : det_poses) {
+        // polt_pose_err(img, det, pose);  // 画出tag pose的err
         // plot_tag_det(img, det);           // 画出tag边框和id
         // plot_pose_axis(img, info, pose);  // 画出tag的坐标系，RGB分别为xyz轴
-        plot_pose_cube(img, info, pose, false);  // 画出tag的3d方块
+        plot_pose_cube(img, info, pose);  // 画出tag的3d方块
     }
 }
 
