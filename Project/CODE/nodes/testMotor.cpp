2825e575f30f243d3b0f3020f62a7164ac4e99a9
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-21 14:59:23 +0800

    改一下调试的pwm增量

--- Project/CODE/nodes/testMotor.cpp
+++ Project/CODE/nodes/testMotor.cpp
@@ -3,12 +3,13 @@
 
 static void testMotorThreadEntry(void*) {
     int32_t duty = 5000;
+    constexpr int32_t delta = 100;
     for (;;) {
         ips.printf("duty: %d\n", duty);
         rt_thread_mdelay(100);
-        if (btn_c4.pressing()) duty += 100;
+        if (btn_c4.pressing()) duty += delta;
         else if (btn_c26.pressing())
-            duty -= 10;
+            duty -= delta;
         else
             continue;
         motorDrvL1.setPWM(duty);
