a204087bede37618bb19b00a24de7a9726b98e02
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-21 21:45:28 +0800

    无线串口通讯弄了设置电机pwm的

--- Project/CODE/nodes/wireless.cpp
+++ Project/CODE/nodes/wireless.cpp
@@ -2,20 +2,35 @@
 //
 #include "devices.hpp"
 
-inline void SystemReset() { NVIC_SystemReset(); }
+static uint8_t id;
+
+inline void SystemReset() {
+    beep.set(false);
+    NVIC_SystemReset();
+}
+
+inline void SetMotorPwm() {
+    int32_t speed;
+    if (!(wireless.getchar(id) && wireless.getData<float>(speed))) return;
+    switch (id) {
+    case 0: motorDrvL1.setPWM(speed); break;
+    case 1: motorDrvL2.setPWM(speed); break;
+    case 2: motorDrvR1.setPWM(speed); break;
+    case 3: motorDrvR2.setPWM(speed); break;
+    }
+    beep.set(false);
+}
 
 static void wirelessEntry() {
-    SerialIO::TxUtil<float, 3> data("recvTest");
     for (;;) {
         wireless.waitHeader();
-        int arr[3];
-        if (wireless.getArr<float, 3>(arr)) {
-            if (data.txFinished()) {
-                data.setArr(arr);
-                wireless.send(data);
-            }
+        beep.set(true);
+        if (!wireless.getchar(id)) continue;
+        switch (id) {
+        case 0: SystemReset(); break;
+        case 1: SetMotorPwm(); break;
         }
     }
 }
 
-bool wirelessNode() { return FuncThread(wirelessEntry, "wireless", 1024, 0, 20); }
+bool wirelessNode() { return FuncThread(wirelessEntry, "wireless", 1024, 0); }
