9e1bfb7725db5c0400bb62018c20cf9e16cead09
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-24 16:24:40 +0800

    初步搞了MotorCtrl (不知道能不能用)

---
+++ Project/CODE/driver/MotorCtrl.hpp
@@ -0,0 +1,36 @@
+#ifndef _MotorCtrl_hpp
+#define _MotorCtrl_hpp
+
+#include "Encoder.hpp"
+#include "MotorDRV.hpp"
+#include "controller/LADRC.hpp"
+
+class MotorCtrl {
+    const MotorDRV& _motor;
+    const Encoder& _encoder;
+    controller::LADRC2& _controller;
+    float _u_prev, _y_desired;
+
+ public:
+    MotorCtrl(const MotorDRV& motor, const Encoder& encoder, controller::LADRC2& controller)
+        : _motor(motor), _encoder(encoder), _controller(controller) {
+        reset();
+    }
+    void reset() { _u_prev = _y_desired = 0; }
+
+    void setTargetSpeed(float speed) { _y_desired = speed; }
+    float getTargetSpeed() const { return _y_desired; }
+    float getOutput() const { return _u_prev; }
+
+    void update() {
+        float u = _controller.update(_u_prev, _encoder.get(), _y_desired);
+        _u_prev = _motor.setPWM(u);
+    }
+
+    void reset() {
+        _controller.reset();
+        _u_prev = _y_desired = 0;
+    }
+};
+
+#endif  // _MotorCtrl_hpp
