0915178b4c67f0d45e53b6062459b664a4b3142d
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-24 10:57:04 +0800

    弄了FuncTimer

--- Project/CODE/utils/FuncThread.hpp
+++ Project/CODE/utils/FuncThread.hpp
@@ -8,4 +8,6 @@
 bool FuncThread(std::function<void()> func, const char* name, rt_uint32_t stack_size = 2048,
                 rt_uint8_t priority = (RT_THREAD_PRIORITY_MAX * 2) / 3, rt_uint32_t tick = 20);
 
+bool FuncTimer(std::function<void()> func, const char* name, rt_uint32_t period_tick);
+
 #endif  // _Thread_hpp
--- Project/CODE/utils/src/FuncThread.cpp
+++ Project/CODE/utils/src/FuncThread.cpp
@@ -26,3 +26,26 @@ bool FuncThread(std::function<void()> func, const char* name, rt_uint32_t stack_
     FuncWrapper* wrapper = new FuncWrapper(func);
     return wrapper->start(name, stack_size, priority, tick);
 }
+
+bool FuncTimer(std::function<void()> func, const char* name, rt_uint32_t period_tick) {
+    class FuncWrapper {
+        const std::function<void()> _func;
+        rt_timer_t _timer;
+        static void funcWrapper(void* self) { ((FuncWrapper*)self)->func(); }
+
+     public:
+        FuncWrapper(std::function<void()> func) : _func(func) {}
+        bool start(const char* name, rt_uint32_t period_tick) {
+            _timer = rt_timer_create(name, funcWrapper, this, period_tick, RT_TIMER_FLAG_PERIODIC | RT_TIMER_FLAG_HARD_TIMER);
+            if (_timer) {
+                if (rt_timer_start(_timer) == RT_EOK) return true;
+                rt_timer_delete(_timer);
+            }
+            delete this;
+            return false;
+        }
+        void func() { _func(); }
+    };
+    FuncWrapper* wrapper = new FuncWrapper(func);
+    return wrapper->start(name, period_tick);
+}
