c1219ec70bbaedd87ee312b8e8a55222cd90fc26
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-24 10:14:07 +0800

    Qtimer弄了个可以反向

--- Project/CODE/driver/QTimer.hpp
+++ Project/CODE/driver/QTimer.hpp
@@ -9,11 +9,16 @@ class QTimer {
  private:
     const QTIMERN_enum Qtimern;
     const QTIMER_PIN_enum A, B;
+    const bool invert;
 
  public:
-    QTimer(QTIMERN_enum qtimern, QTIMER_PIN_enum phaseA, QTIMER_PIN_enum phaseB) : Qtimern(qtimern), A(phaseA), B(phaseB) {}
+    QTimer(QTIMERN_enum qtimern, QTIMER_PIN_enum phaseA, QTIMER_PIN_enum phaseB, bool inverted)
+        : Qtimern(qtimern), A(phaseA), B(phaseB), invert(inverted) {}
     void init() const { qtimer_quad_init(Qtimern, A, B); }
-    int16 get() const { return qtimer_quad_get(Qtimern, A); }
+    int16 get() const {
+        int16 res = qtimer_quad_get(Qtimern, A);
+        return invert ? -res : res;
+    }
     void reset() const { qtimer_quad_clear(Qtimern, A); }
 };
 
