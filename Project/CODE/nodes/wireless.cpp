536887f14d79d808624fed54d404df3dcae35154
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-25 20:32:19 +0800

    这里不用限幅(虽然没啥影响)

--- Project/CODE/nodes/wireless.cpp
+++ Project/CODE/nodes/wireless.cpp
@@ -13,7 +13,7 @@ inline void SetMotorPwm() {
     float speed;
     if (!wireless.getchar(id)) return;
     if (id == 4) {
-        motorDrvL1.setPWM_Limit(0), motorDrvL2.setPWM_Limit(0), motorDrvR1.setPWM_Limit(0), motorDrvR2.setPWM_Limit(0);
+        motorDrvL1.setPWM(0), motorDrvL2.setPWM(0), motorDrvR1.setPWM(0), motorDrvR2.setPWM(0);
     } else {
         if (!wireless.getData<float>(speed)) return;
         switch (id) {
