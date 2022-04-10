9b9c013444ad5f7157628db86baac504ca26ae5f
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-04-10 09:45:56 +0800

    剩下两个串口也测

--- Project/CODE/nodes/uartMasterTest.cpp
+++ Project/CODE/nodes/uartMasterTest.cpp
@@ -24,8 +24,28 @@ static void uart3RecvEntry() {
         }
 }
 
+static void uart4RecvEntry() {
+    uint8_t ch;
+    for (;;)
+        if (uart4.getchar(ch)) {
+            rt_mutex_take(io_lock, RT_WAITING_FOREVER);
+            rt_kprintf("4: %c\r\n", ch);
+            rt_mutex_release(io_lock);
+        }
+}
+
+static void uart5RecvEntry() {
+    uint8_t ch;
+    for (;;)
+        if (uart5.getchar(ch)) {
+            rt_mutex_take(io_lock, RT_WAITING_FOREVER);
+            rt_kprintf("5: %c\r\n", ch);
+            rt_mutex_release(io_lock);
+        }
+}
+
 static void wirelessEntry() {
-    static SerialIO::TxUtil<float, 1> uart2tx("uart2tx"), uart3tx("uart3tx");
+    static SerialIO::TxUtil<float, 1> uart2tx("uart2tx"), uart3tx("uart3tx"), uart4tx("uart4tx"), uart5tx("uart5tx");
     for (;;) {
         wireless.waitHeader();
         uint8_t id;
@@ -42,12 +62,23 @@ static void wirelessEntry() {
             uart3tx.setAll(t);
             uart3.send(uart3tx);
         }
+        if (uart4tx.txFinished()) {
+            uart4tx.setAll(t);
+            uart4.send(uart4tx);
+        }
+        if (uart5tx.txFinished()) {
+            uart5tx.setAll(t);
+            uart5.send(uart5tx);
+        }
     }
 }
 
 bool uartMasterTest() {
     io_lock = rt_mutex_create("io_lock", RT_IPC_FLAG_FIFO);
     if (io_lock == RT_NULL) return false;
-    return FuncThread(wirelessEntry, "wireless", 1024, 0) && FuncThread(uart2RecvEntry, "uart2Recv", 512, 1) &&
-           FuncThread(uart3RecvEntry, "uart3Recv", 512, 1);
+    return FuncThread(wirelessEntry, "wireless", 1024, 0) &&   //
+           FuncThread(uart2RecvEntry, "uart2Recv", 512, 1) &&  //
+           FuncThread(uart3RecvEntry, "uart3Recv", 512, 1) &&  //
+           FuncThread(uart4RecvEntry, "uart4Recv", 512, 1) &&  //
+           FuncThread(uart5RecvEntry, "uart5Recv", 512, 1);
 }
--- Project/USER/src/main.cpp
+++ Project/USER/src/main.cpp
@@ -18,7 +18,7 @@ int main(void) {
 
     // imgUSBXferNode();
     // apriltagDetectNode();
-    // motorControlNode();
+    motorControlNode();
 
     // testMotorNode();
     // testQTimerNode();
