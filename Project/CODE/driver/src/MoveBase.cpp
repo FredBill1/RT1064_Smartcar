bb7e95af749eaa4d7b71f322327a836806581066
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-05-07 16:13:36 +0800

    goal的event弄对了

--- Project/CODE/driver/src/MoveBase.cpp
+++ Project/CODE/driver/src/MoveBase.cpp
@@ -1,5 +1,10 @@
 #include "MoveBase.hpp"
 
+enum class GoalEventFlag : rt_uint32_t {
+    reached = 1 << 0,   // 到达目标
+    disabled = 1 << 1,  // 设置禁用
+};
+
 MoveBase::MoveBase() {
     _goal.reached = true;
     rt_event_init(&_reachedEvent, "GoalReached", RT_IPC_FLAG_PRIO);
@@ -7,7 +12,7 @@ MoveBase::MoveBase() {
 
 void MoveBase::set_enabled(bool enabled) {
     _enabledLoader.store(enabled);
-    if (!enabled) rt_event_send(&_reachedEvent, 1);
+    if (!enabled) rt_event_send(&_reachedEvent, (rt_uint32_t)GoalEventFlag::disabled);
 }
 
 bool MoveBase::get_enabled() {
@@ -28,14 +33,15 @@ const MoveBase::Goal& MoveBase::get_goal() {
 bool MoveBase::get_reached() { return get_goal().reached; }
 void MoveBase::set_reached(bool reached) {
     _goal.reached = reached;
-    rt_event_send(&_reachedEvent, 1);
+    if (reached) rt_event_send(&_reachedEvent, (rt_uint32_t)GoalEventFlag::reached);
 }
 
 bool MoveBase::wait_for_result() {
     if (!get_enabled()) return false;
-    if (get_reached()) return true;
-    rt_event_recv(&_reachedEvent, 1, RT_EVENT_FLAG_OR | RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, RT_NULL);
-    return get_enabled();
+    rt_uint32_t res;
+    rt_event_recv(&_reachedEvent, (rt_uint32_t)GoalEventFlag::reached | (rt_uint32_t)GoalEventFlag::disabled,
+                  RT_EVENT_FLAG_OR | RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &res);
+    return res == (rt_uint32_t)GoalEventFlag::reached;
 }
 
 bool MoveBase::new_goal() {
