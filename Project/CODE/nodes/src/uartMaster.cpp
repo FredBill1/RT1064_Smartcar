a3436d8ed80cb569a74f8ca2e65a7e15a015e722
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-06-01 22:10:44 +0800

    收到0个rect时也发到上位机

--- Project/CODE/nodes/src/uartMaster.cpp
+++ Project/CODE/nodes/src/uartMaster.cpp
@@ -42,28 +42,28 @@ static inline void recvRect() {
     if (id & 1 || rect_cnt > max_rect_cnt) return;
     if (!slave_uart.getArr<float, max_rect_cnt * 2>(rects[0], rect_cnt * 2)) return;
     beep.set(false);
-    if (!rect_cnt) return;
+    if (rect_cnt) {
+        MoveBase::State state;
+        moveBase.get_state(state);
+        float target[2];
+        if (!masterGlobalVars.get_rectTarget(target)) return;
+        float sy = std::sin(state.yaw()), cy = std::cos(state.yaw());
 
-    MoveBase::State state;
-    moveBase.get_state(state);
-    float target[2];
-    if (!masterGlobalVars.get_rectTarget(target)) return;
-    float sy = std::sin(state.yaw()), cy = std::cos(state.yaw());
-
-    float min_dist = 1e9;
-    float res_x, res_y;
-    for (int i = 0; i < rect_cnt; ++i) {
-        float x = target[0] - rects[i][0] * cy + rects[i][1] * sy;
-        float y = target[1] - rects[i][0] * sy - rects[i][1] * cy;
-        float dx = x - state.x(), dy = y - state.y();
-        float dist = std::sqrt(dx * dx + dy * dy);
-        if (dist < min_dist) {
-            min_dist = dist;
-            res_x = x, res_y = y;
+        float min_dist = 1e9;
+        float res_x, res_y;
+        for (int i = 0; i < rect_cnt; ++i) {
+            float x = target[0] - rects[i][0] * cy + rects[i][1] * sy;
+            float y = target[1] - rects[i][0] * sy - rects[i][1] * cy;
+            float dx = x - state.x(), dy = y - state.y();
+            float dist = std::sqrt(dx * dx + dy * dy);
+            if (dist < min_dist) {
+                min_dist = dist;
+                res_x = x, res_y = y;
+            }
         }
+        pose_kalman::T res[2]{res_x, res_y};
+        pose_kalman::kf.enqueMeasurement(pose_kalman::MeasurementType::Rect, res, timestamp_us);
     }
-    pose_kalman::T res[2]{res_x, res_y};
-    pose_kalman::kf.enqueMeasurement(pose_kalman::MeasurementType::Rect, res, timestamp_us);
 
     if (rect_tx.txFinished()) {
         rect_tx.setArr(rects[0], rect_cnt * 2);
