64753265efe98a563adb7480950f238c5721f4ef
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-07-01 11:59:03 +0800

    先瞎改一个

--- Project/USER/inc/localPlannerParam.hpp
+++ Project/USER/inc/localPlannerParam.hpp
@@ -7,7 +7,7 @@ constexpr pose_kalman::LocalPlanner::Params localPlannerParam{
     .vel_lim_xy = 2,
     .vel_lim_yaw = 6,
     .acc_lim_xy = 0.8,
-    .acc_lim_yaw = 2,
+    .acc_lim_yaw = 2.5,
     .dt_ref = 0.3,
 };
 }
