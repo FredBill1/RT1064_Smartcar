b84a3fafadc340623fb0c3c53061760d22147a69
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-08-18 21:59:33 +0800

    机械臂两个关节设角度的时候加锁，防止脱节

--- Project/CODE/driver/src/ArmDrv.cpp
+++ Project/CODE/driver/src/ArmDrv.cpp
@@ -3,6 +3,7 @@
 #include <rtthread.h>
 
 #include "devices.hpp"
+#include "utils/InterruptGuard.hpp"
 
 void ArmDrv::reset() const {
     for (int i = 0; i < 4; ++i) magnets[i].set(true);
@@ -11,46 +12,76 @@ void ArmDrv::reset() const {
 
 void ArmDrv::initial_pose() const {
     magnets[3].set(true);
-    srv_l.set(150);
-    srv_r.set(90);
+    {
+        InterruptGuard guard;
+        srv_l.set(150);
+        srv_r.set(90);
+    }
 }
 void ArmDrv::pick() const {
-    srv_l.set(53);
-    srv_r.set(40);
+    {
+        InterruptGuard guard;
+        srv_l.set(53);
+        srv_r.set(40);
+    }
     rt_thread_mdelay(550);
 }
 void ArmDrv::before_place() const {
-    srv_l.set(120);
-    srv_r.set(172);
+    {
+        InterruptGuard guard;
+        srv_l.set(120);
+        srv_r.set(172);
+    }
     rt_thread_mdelay(700);
 }
 void ArmDrv::place(int index) const {
     switch (index) {
     case 0: {
-        srv_l.set(110);
-        srv_r.set(195);
+        {
+            InterruptGuard guard;
+            srv_l.set(110);
+            srv_r.set(208);
+        }
         rt_thread_mdelay(200);
-        srv_l.set(106);
-        srv_r.set(224);
+        {
+            InterruptGuard guard;
+            srv_l.set(106);
+            srv_r.set(224);
+        }
         rt_thread_mdelay(200);
     } break;
     case 1: {
-        srv_l.set(115);
-        srv_r.set(202);
+        {
+            InterruptGuard guard;
+            srv_l.set(110);
+            srv_r.set(205);
+        }
         rt_thread_mdelay(200);
-        srv_l.set(129);
-        srv_r.set(215);
+        {
+            InterruptGuard guard;
+            srv_l.set(135);
+            srv_r.set(205);
+        }
         rt_thread_mdelay(200);
     } break;
     case 2: {
-        srv_l.set(130);
-        srv_r.set(175);
+        {
+            InterruptGuard guard;
+            srv_l.set(130);
+            srv_r.set(175);
+        }
         rt_thread_mdelay(150);
-        srv_l.set(135);
-        srv_r.set(190);
+        {
+            InterruptGuard guard;
+            srv_l.set(132);
+            srv_r.set(195);
+        }
         rt_thread_mdelay(100);
-        srv_l.set(146);
-        srv_r.set(200);
+        {
+            InterruptGuard guard;
+            srv_l.set(146);
+            srv_r.set(200);
+        }
         rt_thread_mdelay(200);
     } break;
     }
